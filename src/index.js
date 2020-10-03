4e97372631fec6c64ba584b93fd9b6e7e9dcedc0
Quan Xue <33763102+quan-xue@users.noreply.github.com>
2020-10-03 14:11:59 +0800

    Challenge done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img = "https://picsum.photos/200?grayscale";
+
+//Camel case for attributes
+
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1 className="heading">My Favourite Foods</h1>
+    <img src={img} />
+    <img
+      className="food"
+      src="https://rasamalaysia.com/wp-content/uploads/2011/07/curry-laksa.jpg"
+      alt="laksa"
+    ></img>
+    <img
+      className="food"
+      src="https://www.visitsingapore.com/content/dam/desktop/global/dining-drinks-singapore/local-dishes/friedkwayteow-carousel1-640x640.jpg"
+      alt="kway teow"
+    ></img>
   </div>,
   document.getElementById("root")
 );
